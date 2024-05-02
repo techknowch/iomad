f74ff0502b49394e7783ec4eab0a964ac8b3b927
Ali Naqvi <techknowch@protonmail.com>
2024-05-02 21:54:39 +0500

    Update settings.php

--- theme/noba/settings.php
+++ theme/noba/settings.php
@@ -1,79 +1,111 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+/**
+ * @package   theme_boost
+ * @copyright 2016 Ryan Wyllie
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-// This line protects the file from being accessed by a URL directly.                                                               
-defined('MOODLE_INTERNAL') || die();                                                                                                
-                                                                                                                                    
-// This is used for performance, we don't need to know about these settings on every page in Moodle, only when                      
-// we are looking at the admin settings pages.                                                                                      
-if ($ADMIN->fulltree) {                                                                                                             
-                                                                                                                                    
-    // Boost provides a nice setting page which splits settings onto separate tabs. We want to use it here.                         
-    $settings = new theme_boost_admin_settingspage_tabs('themesettingphoto', get_string('configtitle', 'theme_photo'));             
-                                                                                                                                    
-    // Each page is a tab - the first is the "General" tab.                                                                         
-    $page = new admin_settingpage('theme_photo_general', get_string('generalsettings', 'theme_photo'));                             
-                                                                                                                                    
-    // Replicate the preset setting from boost.                                                                                     
-    $name = 'theme_photo/preset';                                                                                                   
-    $title = get_string('preset', 'theme_photo');                                                                                   
-    $description = get_string('preset_desc', 'theme_photo');                                                                        
-    $default = 'default.scss';                                                                                                      
-                                                                                                                                    
-    // We list files in our own file area to add to the drop down. We will provide our own function to                              
-    // load all the presets from the correct paths.                                                                                 
-    $context = context_system::instance();                                                                                          
-    $fs = get_file_storage();                                                                                                       
-    $files = $fs->get_area_files($context->id, 'theme_photo', 'preset', 0, 'itemid, filepath, filename', false);                    
-                                                                                                                                    
-    $choices = [];                                                                                                                  
-    foreach ($files as $file) {                                                                                                     
-        $choices[$file->get_filename()] = $file->get_filename();                                                                    
-    }                                                                                                                               
-    // These are the built in presets from Boost.                                                                                   
-    $choices['default.scss'] = 'default.scss';                                                                                      
-    $choices['plain.scss'] = 'plain.scss';                                                                                          
-                                                                                                                                    
-    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);                                     
-    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
-    $page->add($setting);                                                                                                           
-                                                                                                                                    
-    // Preset files setting.                                                                                                        
-    $name = 'theme_photo/presetfiles';                                                                                              
-    $title = get_string('presetfiles','theme_photo');                                                                               
-    $description = get_string('presetfiles_desc', 'theme_photo');                                                                   
-                                                                                                                                    
-    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,                                         
-        array('maxfiles' => 20, 'accepted_types' => array('.scss')));                                                               
-    $page->add($setting);     
+defined('MOODLE_INTERNAL') || die();
 
-    // Variable $brand-color.                                                                                                       
-    // We use an empty default value because the default colour should come from the preset.                                        
-    $name = 'theme_photo/brandcolor';                                                                                               
-    $title = get_string('brandcolor', 'theme_photo');                                                                               
-    $description = get_string('brandcolor_desc', 'theme_photo');                                                                    
-    $setting = new admin_setting_configcolourpicker($name, $title, $description, '');                                               
-    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
-    $page->add($setting);                                                                                                           
-                                                                                                                                    
-    // Must add the page after definiting all the settings!                                                                         
-    $settings->add($page);                                                                                                          
-                                                                                                                                    
-    // Advanced settings.                                                                                                           
-    $page = new admin_settingpage('theme_photo_advanced', get_string('advancedsettings', 'theme_photo'));                           
-                                                                                                                                    
-    // Raw SCSS to include before the content.                                                                                      
-    $setting = new admin_setting_configtextarea('theme_photo/scsspre',                                                              
-        get_string('rawscsspre', 'theme_photo'), get_string('rawscsspre_desc', 'theme_photo'), '', PARAM_RAW);                      
-    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
-    $page->add($setting);                                                                                                           
-                                                                                                                                    
-    // Raw SCSS to include after the content.                                                                                       
-    $setting = new admin_setting_configtextarea('theme_photo/scss', get_string('rawscss', 'theme_photo'),                           
-        get_string('rawscss_desc', 'theme_photo'), '', PARAM_RAW);                                                                  
-    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
-    $page->add($setting);                                                                                                           
-                                                                                                                                    
-    $settings->add($page);                                                                                                          
+if ($ADMIN->fulltree) {
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingboost', get_string('configtitle', 'theme_boost'));
+    $page = new admin_settingpage('theme_boost_general', get_string('generalsettings', 'theme_boost'));
+
+    // Unaddable blocks.
+    // Blocks to be excluded when this theme is enabled in the "Add a block" list: Administration, Navigation, Courses and
+    // Section links.
+    $default = 'navigation,settings,course_list,section_links';
+    $setting = new admin_setting_configtext('theme_boost/unaddableblocks',
+        get_string('unaddableblocks', 'theme_boost'), get_string('unaddableblocks_desc', 'theme_boost'), $default, PARAM_TEXT);
+    $page->add($setting);
+
+    // Preset.
+    $name = 'theme_boost/preset';
+    $title = get_string('preset', 'theme_boost');
+    $description = get_string('preset_desc', 'theme_boost');
+    $default = 'default.scss';
+
+    $context = context_system::instance();
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($context->id, 'theme_boost', 'preset', 0, 'itemid, filepath, filename', false);
+
+    $choices = [];
+    foreach ($files as $file) {
+        $choices[$file->get_filename()] = $file->get_filename();
+    }
+    // These are the built in presets.
+    $choices['default.scss'] = 'default.scss';
+    $choices['plain.scss'] = 'plain.scss';
+
+    $setting = new admin_setting_configthemepreset($name, $title, $description, $default, $choices, 'boost');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Preset files setting.
+    $name = 'theme_boost/presetfiles';
+    $title = get_string('presetfiles','theme_boost');
+    $description = get_string('presetfiles_desc', 'theme_boost');
+
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
+        array('maxfiles' => 20, 'accepted_types' => array('.scss')));
+    $page->add($setting);
+
+    // Background image setting.
+    $name = 'theme_boost/backgroundimage';
+    $title = get_string('backgroundimage', 'theme_boost');
+    $description = get_string('backgroundimage_desc', 'theme_boost');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'backgroundimage');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Login Background image setting.
+    $name = 'theme_boost/loginbackgroundimage';
+    $title = get_string('loginbackgroundimage', 'theme_boost');
+    $description = get_string('loginbackgroundimage_desc', 'theme_boost');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'loginbackgroundimage');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // We use an empty default value because the default colour should come from the preset.
+    $name = 'theme_boost/brandcolor';
+    $title = get_string('brandcolor', 'theme_boost');
+    $description = get_string('brandcolor_desc', 'theme_boost');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, '');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Must add the page after definiting all the settings!
+    $settings->add($page);
+
+    // Advanced settings.
+    $page = new admin_settingpage('theme_boost_advanced', get_string('advancedsettings', 'theme_boost'));
+
+    // Raw SCSS to include before the content.
+    $setting = new admin_setting_scsscode('theme_boost/scsspre',
+        get_string('rawscsspre', 'theme_boost'), get_string('rawscsspre_desc', 'theme_boost'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Raw SCSS to include after the content.
+    $setting = new admin_setting_scsscode('theme_boost/scss', get_string('rawscss', 'theme_boost'),
+        get_string('rawscss_desc', 'theme_boost'), '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    $settings->add($page);
 }
