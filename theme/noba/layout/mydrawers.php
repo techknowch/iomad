4fe32b3c65e3bfa251d28b4fca6ab498e23e5e2d
Ali Naqvi <techknowch@protonmail.com>
2024-05-03 17:39:33 +0500

    update my dashboard

---
+++ theme/noba/layout/mydrawers.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A drawer based layout for the boost theme.
+ *
+ * @package   theme_boost
+ * @copyright 2021 Bas Brands
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Output the main content of the page.
+echo $OUTPUT->main_content();
+
+// The rest of your layout code goes here...
+
+$templatecontext['custom_message'] = '<p>This is my custom message.</p>';
+
+echo $OUTPUT->render_from_template('theme_noba/mydrawers', $templatecontext);
+
