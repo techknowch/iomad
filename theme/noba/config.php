10d4a7f54d90139aba14d1d75b11aa01526e0395
Ali Naqvi <techknowch@protonmail.com>
2024-05-07 19:33:25 +0500

    Update config.php

--- theme/noba/config.php
+++ theme/noba/config.php
@@ -70,7 +70,7 @@ $THEME->layouts = [
  //   ),
     // My dashboard page.
     'mydashboard' => array(
-        'file' => 'mydrawers.php',
+        'file' => 'mycustompage.php',
         'regions' => array('side-pre'),
         'defaultregion' => 'side-pre',
         'options' => array('nonavbar' => true, 'langmenu' => true),
