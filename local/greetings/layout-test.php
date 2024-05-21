6618d6133472622572b24ed10cbfdf7a4b7cf1dc
Ali Naqvi <techknowch@protonmail.com>
2024-05-21 18:57:13 +0500

    Create layout-test.php

---
+++ local/greetings/layout-test.php
@@ -0,0 +1,14 @@
+<?php
+require_once('../../config.php');
+
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/greetings/layout-test.php'));
+
+$PAGE->set_title(get_string('pluginname', 'local_greetings'));
+$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
+
+echo $OUTPUT->header();
+// Your content goes here. We will just echo some HTML for simplicity.
+echo '<div class="box border p-3">Here is some content but it can be anything else, too.</div>';
+echo $OUTPUT->footer();
