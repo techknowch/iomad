d3f8b78b83368ce6a10e511c8fd6a80399ce4d2e
Ali Naqvi <techknowch@protonmail.com>
2024-05-21 20:07:07 +0500

    Create index.php

---
+++ local/greetings/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// local/greetings/index.php
+
+require_once(__DIR__ . '/../../config.php');
+require_login();
+
+$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
+$PAGE->set_context(context_system::instance());
+$PAGE->set_title(get_string('greetingstitle', 'local_greetings'));
+$PAGE->set_heading(get_string('greetingstitle', 'local_greetings'));
+
+// Example messages array (replace with actual data retrieval logic)
+$messages = [
+    ['id' => 1, 'content' => 'Hello, World!', 'author' => 'User1'],
+    ['id' => 2, 'content' => 'Greetings!', 'author' => 'User2']
+];
+
+$renderable = new \local_greetings\output\index_page($messages);
+$output = $PAGE->get_renderer('local_greetings');
+echo $output->header();
+echo $output->render($renderable);
+echo $output->footer();
